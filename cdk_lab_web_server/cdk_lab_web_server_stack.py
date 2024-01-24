6564607404ee97e71704b836d1122f0245da4cdd
EC2 Default User <ec2-user@ip-172-31-24-189.ec2.internal>
2024-01-24 07:16:56 +0000

    move VPC to network stack. Reference VPC in server stack

--- cdk_lab_web_server/cdk_lab_web_server_stack.py
+++ cdk_lab_web_server/cdk_lab_web_server_stack.py
@@ -16,17 +16,10 @@ dirname = os.path.dirname(__file__)
         
 class CdkLabWebServerStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, cdk_lab_vpc: ec2.Vpc, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
-        
-        # Create a VPC. CDK by default creates and attaches internet gateway for VPC
-        cdk_lab_vpc = ec2.Vpc(self, "cdk_lab_vpc", 
-                            ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/16"),
-                            subnet_configuration=[ec2.SubnetConfiguration(name="PublicSubnet01",subnet_type=ec2.SubnetType.PUBLIC)]
-                            )
-        
 
         # Instance Role and SSM Managed Policy
         InstanceRole = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
